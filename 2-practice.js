9cfc003cbd42d607b889d679777b4a075e577c2b
samlutz <sammlutz@gmail.com>
2019-08-08 16:13:47 -0400

    new algorithm added and solved

--- 2-practice.js
+++ 2-practice.js
@@ -19,4 +19,48 @@
 // Your task is to write a function maskify, which changes all but the last four characters into '#'.
 // *************************************************************************************************************************************************************
 
+// PSEUDOCODE
+
+//     PARAMETERS
+//         str
+
+//     VARIABLES
+//         arr = str.split()
+//         lastFour;
+//         toEncrypt;
+//         encrypted;
+//         newArr
+
+        
+//     split str into an array 
+//     slice the last four items off and store in lastFour
+//     store the rest of the array in toEncrypt
+//     loop over the toEncrypt array and replace each item with #
+//     store new array in encrypted var 
+//     join encrypted with lastFour 
+
+//      
+
+
+const maskify = str => {
+    str = str.split('')
+    console.log(str)
+    let lastFour = str.slice(-4)
+    console.log(lastFour)
+    const toEncrypt = str.slice(0,-4)
+    console.log(toEncrypt)
+    const encrypted = toEncrypt.map(item => item = "#")
+    console.log(encrypted)
+    const newArr = encrypted.concat(lastFour)
+    console.log(newArr)
+    
+}
+
+maskify('1234567')
+
+// const arr = [1,2,3,4]
+
+// const newArr = arr.map(item => item = "#")
+
+// console.log(newArr)
 
