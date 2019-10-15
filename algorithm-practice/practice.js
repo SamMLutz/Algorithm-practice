cea96e2b4ca61a4285640d5ad736d90ab2e8f964
samlutz <sammlutz@gmail.com>
2019-10-15 16:32:16 -0400

    refactor alogorithms

--- algorithm-practice/practice.js
+++ algorithm-practice/practice.js
@@ -600,14 +600,16 @@
 //   return result;
 // }
 
-// log(factorialize(5));
+// factorialize(5);
 
 // function factorialize(num) {
 //   if (num === 0 || num === 1)
 //     return 1;
 //   for (var i = num - 1; i >= 1; i--) {
+//     console.log("num: ", num)
 //     num *= i;
 //   }
+//   console.log("num: ", num)
 //   return num;
 // }
 
